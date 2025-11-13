f5fd6dbda15f4e07ad82aad98cf72e47ea075f7a
linhexi <2332019407@qq.com>
2025-11-13 16:10:22 +0800

    vue3项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div class="button-row">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
